e901eb03223773022570404cc05a6ded4267fc16
cliftoncallender <clifton.callender@gmail.com>
2018-07-24 21:46:43 -0400

    Improved AI
    Now searches against all 8 snake patterns.
    
    Throttles search depth based on number of empty cells.

--- js/ai.js
+++ js/ai.js
@@ -1,12 +1,3 @@
-function moveName(move) {
- return {
-    0: 'up',
-    1: 'right',
-    2: 'down',
-    3: 'left'
-  }[move];
-}
-
 // performs a search and returns the best move
 function AI_getBest(grid, debug) {
   Player = Snake
@@ -16,23 +7,110 @@ function AI_getBest(grid, debug) {
   return {move: playerMove};
 }
 
+// AI based on expectimax and snake-pattern weight function.
 class Snake {
   constructor() {
-    this.weights = this.makeSnakeWeights();
+    this.size = 4;
+    this.snake = [0, 1, 2, 3, 7, 6, 5, 4, 8, 9, 10, 11, 15, 14, 13, 12];
+    this.weights = this.makeWeights();
+    this.allWeights = this.makeAllWeights(this.makeSnake(this.snake, this.weights))
+    this.depth = 5;
   }
 
-  makeSnakeWeights() {
-    var snake = [[0, 7, 8, 15], [1, 6, 9, 14], [2, 5, 10, 13], [3, 4, 11, 12]];
-    var  weights = new Array(4);
-    for (var i = 0; i < weights.length; i++){
-      weights[i] = new Array(4);
+  makeSnake(snakePattern, weights) {
+    // create empty square matrix
+    var matrix = new Array(this.size);
+    for (var x = 0; x < this.size; x++) {
+      matrix[x] = new Array(this.size);
     }
-    for (x = 0; x < 4; x++) {
-      for (y = 0; y < 4; y++) {
-        weights[x][y] = 0.25 ** snake[x][y];
+
+    // insert weight into matrix in the cell indicated by snakePattern
+    for (var i = 0; i < weights.length; i++) {
+      var x = snakePattern[i] % this.size;
+      var y = Math.floor(snakePattern[i] / this.size);
+      matrix[x][y] = weights[i]
+    }
+
+    return matrix
+  }
+
+  makeWeights() {
+    const ratio = 0.5 // Constant ratio of descending weights.
+    var weights = []
+    for (var i = 0; i < this.size ** 2; i++) {
+      weights[i] = ratio ** i
+    }
+    return weights
+  }
+
+  makeAllWeights(matrix) {
+    // accumulator for all weight matrices
+    var allWeights = [];
+    // matrix and its inversion
+    var flipWeights = [matrix, this.flip(matrix)];
+
+    // add matrix, its inversion, and their rotations to allWeights
+    for (var i = 0; i < flipWeights.length; i++) {
+      for (var j = 0; j < 4; j++) {
+        allWeights.push(this.rotate(flipWeights[i], j))
       }
     }
-    return weights;
+
+    return allWeights;
+  }
+
+  rotate(grid, k) {
+    // create clone of grid
+    var newGrid = new Array(4);
+    for (var x = 0; x < grid.length; x++){
+      newGrid[x] = new Array(4);
+      for (var y = 0; y < grid[x].length; y++) {
+        newGrid[x][y] = grid[x][y]
+      }
+    }
+
+    // rotate the grid by 90 * k degrees
+    for (var i = 0; i < k; i++) {
+      newGrid = this.rotate90(newGrid)
+    }
+
+    return newGrid
+  }
+
+  rotate90(grid) {
+    // rotate grid by 90 degress
+    var xLength = grid.length;
+    var yLength = grid[0].length;
+    var newGrid = new Array();
+
+    for (var x = 0; x < xLength; x++) {
+      newGrid.push([])
+      for (var y = 0; y < yLength; y++) {
+        var newX = y
+        var newY = xLength - x - 1
+        newGrid[x].push(grid[newX][newY])
+      }
+    }
+
+    return newGrid
+  }
+
+  flip(grid) {
+    // flip grid along its NW/SE diagonal
+    var xLength = grid.length;
+    var yLength = grid[0].length;
+    var newGrid = new Array();
+
+    for (var x = 0; x < xLength; x++) {
+      newGrid.push([])
+      for (var y = 0; y < yLength; y++) {
+        var newX = y
+        var newY = x
+        newGrid[x].push(grid[newX][newY])
+      }
+    }
+
+    return newGrid
   }
 
   move(grid) {
@@ -40,10 +118,18 @@ class Snake {
     var bestMove = -1;
     var bestScore = 0;
 
+    const numEmptyCells = grid.availableCells().length;
+
+    var depth
+
+    if (numEmptyCells > 4) depth = this.depth;
+    else if (numEmptyCells > 2) depth = this.depth + 2;
+    else depth = this.depth + 4;
+
     for (var i = 0; i < legalMoves.length; i++) {
       var g = grid.clone();
       g.move(legalMoves[i]);
-      var score = this.expectimax(g)
+      var score = this.expectimax(g, depth)
 
       if (score >= bestScore) {
         bestScore = score;
@@ -53,34 +139,29 @@ class Snake {
     return bestMove;
   }
 
-  dotProduct(grid) {
+  fitness(grid) {
     var total = 0;
-    for (var x = 0; x < 4; x++) {
-      for (var y = 0; y < 4; y++) {
-        if (grid.cells[x][y]) {
-          total += grid.cells[x][y].value * this.weights[x][y];
-          // console.log(grid.cells[x][y].value, this.weights[x][y], grid.cells[x][y].value * this.weights[x][y], total)
+
+    for (var i = 0; i < this.allWeights.length; i++) {
+      var tempTotal = 0
+
+      // dot product of grid with each matrix in this.allWeights
+      for (var x = 0; x < this.size; x++) {
+        for (var y = 0; y < this.size; y++) {
+          if (grid.cells[x][y]) {
+            tempTotal += grid.cells[x][y].value * this.allWeights[i][x][y];
+          }
         }
       }
+      if (tempTotal > total) total = tempTotal;
     }
+
     return total;
   }
 
-  fitness(grid) {
-    return this.dotProduct(grid, this.weights);
-  }
-
   expectimax(grid, depth = 5, playerMove = false) {
-    // console.log("grid: ", grid, "depth: ", depth)
     var alpha = this.fitness(grid);
-    // console.log("alpha: ", alpha)
-    // if (playerMove && grid.legalMoves().length == 0) {
-    //   return -1000000
-    // }
-    // console.log("depth is 0: ", depth == 0)
-    // console.log("player move: ", playerMove)
     if (depth == 0) return alpha;
-    if (depth < 0) fail;
 
     if (playerMove) {
       var legalMoves = grid.getLegalMoves();
@@ -93,9 +174,9 @@ class Snake {
       }
 
     } else {
-      alpha = 0;
+      var alpha = 0;
       var emptyCells = grid.availableCells();
-      var numEmptyCells = emptyCells.length
+      const numEmptyCells = emptyCells.length
 
       for (var i = 0; i < numEmptyCells; i++) {
         var possSpawns = [{value: 2, prob: 1}]; // [{value: 2, prob: 0.9}, {value: 4, prob: 0.1}];
@@ -110,26 +191,3 @@ class Snake {
     return alpha
   }
 }
-
-// class Random {
-//   move(grid) {
-//     var legalMoves = grid.getLegalMoves();
-//     var randomMove = legalMoves[Math.floor(Math.random() * legalMoves.length)];
-//     return randomMove;
-//   }
-// }
-//
-// class MoveHierarchy {
-//   constructor() {
-//     this.moveHierarchy = [0, 3, 1, 2];
-//   }
-//
-//   move(grid) {
-//     for (var i = 0, n = this.moveHierarchy.length; i < n; i++) {
-//       var currentMove = this.moveHierarchy[i];
-//       if (grid.isLegalMove(currentMove)) {
-//         return currentMove;
-//       }
-//     }
-//   }
-// }
